13163969cf44f31db62fe6307180665ca1115328
root <root@i9thomson.localdomain>
2021-09-07 17:22:15 +0200

    before swapping to enumtest branch

--- Docs/User/source/_static/examples/runmartethomson.sh
+++ Docs/User/source/_static/examples/runmartethomson.sh
@@ -1,3 +1,4 @@
 #./MARTeApp.sh -l SPCRealTimeLoader -f ./Configurations/RTThomson-FAT.cfg -m ShotManager:Start
-./MARTeApp.sh -l SPCRealTimeLoader -f ./Configurations/RTThomson-2threads.cfg -m ShotManager:Start
+#DEMUX=0 EXT_UCLK=1 ./MARTeApp.sh -l SPCRealTimeLoader -f ./Configurations/RTThomson-2threads.cfg -m ShotManager:Start
+DEMUX=0 EXT_UCLK=0 ./MARTeApp.sh -l SPCRealTimeLoader -f ./Configurations/RTThomson-2threads.cfg -m ShotManager:Start
 #./MARTeApp.sh -l RealTimeLoader -f ./Configurations/SimulinkWrapperTester.cfg 
